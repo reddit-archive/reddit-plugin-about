20743ffde95d7f395d51b2b1cb1b5d1270e1e33b
Max Goodman <c@chromakode.com>
2012-10-26 14:49:56 -0700

    Add avatar rendering helper script.

---
+++ scripts/render_avatars.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]
+then
+  echo "usage: ./render_avatars.sh SVGDIR"
+  exit
+fi
+
+for svg in $1/*.svg; do
+    target=../reddit_about/public/static/about/avatar/`basename $svg .svg`.png
+    if [ $svg -nt $target ]; then
+        # render oversized and resize with unsharp mask for silky smooth avatars.
+        inkscape -e $target -d 1000 $svg; convert -resize 160x160 $target -unsharp 0x0.5 $target
+    fi
+done
