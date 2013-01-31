2be7e635e41123a042f8b283a86130588d6a6ad8
Max Goodman <c@chromakode.com>
2013-01-31 10:36:49 -0800

    Create rendered avatar file if it doesn't exist.

--- scripts/render_avatars.sh
+++ scripts/render_avatars.sh
@@ -8,7 +8,7 @@ fi
 
 for svg in $1/*.svg; do
     target=../reddit_about/public/static/about/avatar/`basename $svg .svg`.png
-    if [ $svg -nt $target ]; then
+    if [ ! -e $target -o $svg -nt $target ]; then
         # render oversized and resize with unsharp mask for silky smooth avatars.
         inkscape -e $target -d 1000 $svg
         convert -resize 160x160 $target -unsharp 0x0.5 $target
