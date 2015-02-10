521e7b6896be6bc85f03f0d29eb46763403a9211
Matt Lee <matt@reddit.com>
2015-02-10 15:42:55 -0800

    cleanup: use new fireFunnelEvent method.

--- reddit_about/public/static/js/advertising/advertising.js
+++ reddit_about/public/static/js/advertising/advertising.js
@@ -6,11 +6,19 @@
     // added to the link section in the footer (which is markdown pulled from
     // a wiki page), I'm just going to attach events to all of the links on the
     // page containing 'redditadvertising' in the href since it works for both
-    $('a[href*=redditadvertising]').on('click', function() {
+    $('a[href*=redditadvertising]').on('click', function(e) {
+      e.stopPropagation();
       // get the name of the nearest parent element with a selfserve-* css class
       // so the sales team can tell which instance of the link it is
+      var redirectURL = this.href;
       var parentClass = $(this).closest('[class^=selfserve]').attr('class');
-      r.analytics.fireGAEvent('advertising', 'contactLinkClicked', parentClass, this.href);
+      r.analytics.fireFunnelEvent(
+        'advertising',
+        'contactLinkClicked',
+        { label: parentClass },
+        function() {
+          window.location.href = redirectURL;
+        });
     });
   });
 }(r, jQuery);
